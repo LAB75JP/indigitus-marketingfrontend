d44a25fa794328f6ee74fdf110b93f01b7327fdf
Robert Merten <rm.webdevelopment@gmail.com>
2014-02-25 20:55:35 +0100

    Adding vcRecaptcha, Testing jquery.terminal

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,11 @@
 'use strict';
 
-angular.module('indigitusDemonstrationApp', [
+angular.module('indigitusMarketingApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'vcRecaptcha'
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
@@ -20,26 +21,26 @@ angular.module('indigitusDemonstrationApp', [
   });
 
 
-$(document).ready(function () {
-  if ($('#term')) {
-    $('#term').terminal(function (command, term) {
-      if (command !== '') {
-        try {
-          var result = window.eval(command);
-          if (result !== undefined) {
-            term.echo(new String(result));
-          }
-        } catch (e) {
-          term.error(new String(e));
-        }
-      } else {
-        term.echo('');
-      }
-    }, {
-      greetings: 'Javascript Interpreter',
-      name: 'js_demo',
-      height: 200,
-      prompt: 'js> '
-    });
-  }
-});
+//$(document).ready(function () {
+//  if ($('#term').length) {
+//    $('#term').terminal(function (command, term) {
+//      if (command !== '') {
+//        try {
+//          var result = window.eval(command);
+//          if (result !== undefined) {
+//            term.echo(new String(result));
+//          }
+//        } catch (e) {
+//          term.error(new String(e));
+//        }
+//      } else {
+//        term.echo('');
+//      }
+//    }, {
+//      greetings: 'Javascript Interpreter',
+//      name: 'js_demo',
+//      height: 200,
+//      prompt: 'js> '
+//    });
+//  }
+//});
