e9e8c3532feb8e7693473133971d2d1542ff4c6a
Robert Merten <rm.webdevelopment@gmail.com>
2014-02-26 17:39:46 +0100

    Start Instance and Control Panel Routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,19 +5,29 @@ angular.module('indigitusMarketingApp', [
   'ngResource',
   'ngSanitize',
   'ngRoute',
+  'ngAnimate',
+  'ui.bootstrap',
   'vcRecaptcha'
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'partials/main',
-        controller: 'MainCtrl'
+        templateUrl: 'partials/landing_page',
+        controller: 'LandingPageCtrl'
+      })
+      .when('/start_instance', {
+        templateUrl: 'partials/start_instance',
+        controller: 'StartInstanceCtrl'
+      })
+      .when('/control_panel', {
+        templateUrl: 'partials/control_panel',
+        controller: 'ControlPanelCtrl'
       })
       .otherwise({
         redirectTo: '/'
       });
 
-    $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(false);
   });
 
 
