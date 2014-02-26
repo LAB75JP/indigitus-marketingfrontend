b1776800f64b99f326916585fc1a104d7fbcab2a
Robert Merten <rm.webdevelopment@gmail.com>
2014-02-26 17:42:10 +0100

    Landing Page Controller

---
+++ app/scripts/controllers/landing_page.js
@@ -0,0 +1,59 @@
+'use strict';
+
+angular.module('indigitusMarketingApp')
+  .controller('LandingPageCtrl', function ($scope, $http, $location, vcRecaptchaService) {
+
+    console.log($location);
+
+    $scope.alerts = [];
+
+    $scope.closeAlert = function (index) {
+      $scope.alerts.splice(index, 1);
+    };
+
+    $scope.checkCaptcha = function () {
+
+      $scope.alerts = [];
+
+      var data = vcRecaptchaService.data();
+
+
+      if (data.response.length === 0) {
+        $scope.alerts.push({
+          type: 'danger',
+          msg: 'Please answer the captcha!'
+        });
+        return;
+      }
+
+      $http({
+        method: 'POST',
+        url: '/check_recaptcha',
+        data: vcRecaptchaService.data()
+      }).success(function (data) {
+        if (data.err) {
+          $scope.alerts.push({
+            type: 'danger',
+            msg: 'Incorrect Captcha, please try again!'
+          });
+          vcRecaptchaService.reload();
+        }
+        if (data.success) {
+          console.log('IS CORRECT');
+          $location.path('/start_instance').replace();
+          return;
+        }
+        vcRecaptchaService.reload();
+      }).error(function (data) {
+        vcRecaptchaService.reload();
+        $scope.alerts.push({
+          type: 'danger',
+          msg: data.err
+        });
+
+      });
+
+
+    };
+
+  });
