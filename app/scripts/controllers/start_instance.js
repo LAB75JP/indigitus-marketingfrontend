91a3f8741bebc32189688085ae9fb27e1d6f2633
Robert Merten <rm.webdevelopment@gmail.com>
2014-02-28 14:27:12 +0100

    Styling of Landing Page and Start Instance Screens.

--- app/scripts/controllers/start_instance.js
+++ app/scripts/controllers/start_instance.js
@@ -1,19 +1,31 @@
 'use strict';
 
 angular.module('indigitusMarketingApp')
-  .controller('StartInstanceCtrl', function($scope, $http, $location, $timeout, socket) {
+  .controller('StartInstanceCtrl', function ($scope, $http, $location, $timeout, socket) {
     console.log(socket);
     $scope.steps = [];
+
+    $scope.timeLeft = 60;
+
+    var decrease = function () {
+      $scope.timeLeft--;
+      if ($scope.timeLeft > 0) {
+        $timeout(decrease, 1000);
+      }
+
+    };
+    $timeout(decrease, 1000);
+
     socket.emit('instance.start');
 
-    socket.on('instance.step', function(data) {
+    socket.on('instance.step', function (data) {
       console.log(data.step);
       $scope.steps.push(data.step);
     });
 
-    socket.on('instance.ready', function(data) {
+    socket.on('instance.ready', function (data) {
       console.log('ON INSTANCE READY');
-      $location.path('/control_panel').replace();
+      //$location.path('/control_panel').replace();
     });
 
 
