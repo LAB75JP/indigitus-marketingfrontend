fdf7fbc99ddc7761286847183488ed17547aa2a6
Robert Merten <rm.webdevelopment@gmail.com>
2014-03-02 15:50:29 +0100

    Does this work

--- server.js
+++ server.js
@@ -1,10 +1,54 @@
 'use strict';
-
-
-
 var _fs = require('fs');
 
+var Nova = require('openclient').Nova;
 
+var client = new Nova({
+  url: 'http://identity-pub.stage.indigitus.net/v2.0',
+  debug: true
+})
+
+client.authenticate({
+    username: 'marco@lab75.jp',
+    password: 'lab75',
+    project:'00000000marco@lab75.jp'
+  // Callbacks can either be success/error handlers in the options object or
+  // a callback function as the last argument.
+  }, function (err, token) {
+    console.log(err);
+    console.log(token);
+    
+    client.servers.all({
+      endpoint_type: "adminURL",  // Defaults to "publicURL".
+
+      // Callbacks receive the result of the call;
+      success: function (servers) {
+        
+        console.log('Servers');
+        console.log(servers[0].addresses);
+        var serverData = {
+          name: '_TEST_SERVER',
+          imageRef: servers[0].image.links[0].href,
+          flavorRef: servers[0].flavor.links[0].href,
+          networks: [{
+            uuid: 'a7a76164-ef12-4adb-96c0-b703e71f7355'
+          }]
+        };
+        console.log('SERVER DATA', serverData);
+        var newServer = client.servers.create({
+          data: serverData,
+          async: false
+        });
+        console.log(newServer);
+        
+      },
+      error: function (err) {
+        console.error(err);
+      }
+    });
+    
+    
+  });
 /*
  * MAIN SERVER (HTTP)
  */
@@ -102,4 +146,6 @@ var wsserver = null;
 
 
 exports = module.exports = httpserver;
+
+
 
